f7a9bb93d5883c799438e9c9b6b6df9eda0fc623
Muneeb2017 <muneeb.riyaz75@gmail.com>
2021-05-29 18:04:05 +0530

    first

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,74 +1,78 @@
-import { nanoid } from 'nanoid';
+import {
+  nanoid
+} from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: "Muneeb| portfolio", // e.g: 'Name | Developer'
+
+  description: "Welcome to my Portfoilio Website", // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hello, My Name is',
+  name: 'Muneeb Ahmed',
+  subtitle: 'I am a web developer',
+  cta: 'Know more about me',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'I am a CS undergrad from the beautiful valley of Kashmir. I am working on JavaScript and MERN stack',
+  paragraphTwo: 'I have also worked on python and some machine learning projects',
+  paragraphThree: ' Looking forward to work on some very amazing projects!',
+  resume: 'https://drive.google.com/file/d/1mOqZclsNJopbpAKm6uFm5Pg7_9gBMPe4/view?usp=sharing',
+
+
 };
 
 // PROJECTS DATA
-export const projectsData = [
-  {
+export const projectsData = [{
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    
+    title: 'Weather Web app',
+    info: 'A web app using python framework flask to predict weather of a certain area using machine learning algorithms on a dataset',
+
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/Muneeb2017/WeatherPredict-ML', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'project2.jpg',
+    title: 'Photo Gallery App',
+    info: ' A photo gallery app made with ReactJS and firebase ',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://ma-photo-gallery-app.netlify.app/',
+    repo: 'https://github.com/Muneeb2017/photo-gallery-app', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+   
+    title: 'A React TODO app',
+    info: ' App made in React to log your daily todo list . ',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://react-todoapp-648bc.web.app/',
+    repo: 'https://github.com/Muneeb2017/react-todoapp',
+    
   },
+
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'muneeb.riyaz75@gmail.com',
 };
 
 // FOOTER DATA
 export const footerData = {
-  networks: [
-    {
+  networks: [{
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/_muneeb1',
     },
     {
       id: nanoid(),
@@ -78,17 +82,17 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/muneeb-ahmad-420ab7163/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/Muneeb2017',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
